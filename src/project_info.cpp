898a94433a87e26c38d520dd663a3be3e90ca7e4
karei <eetu.kivelae@gmail.com>
2024-02-06 18:30:23 +0200

    Support adding arbitary dependencies

--- src/project_info.cpp
+++ src/project_info.cpp
@@ -47,6 +47,46 @@ cpp::result<void, Error> handle_adding_dependencies(ProjectInfo& project_info) {
     project_info.dependencies = std::vector<Dependency>(default_dependencies.begin(), default_dependencies.end());
   }
 
+  while (true) {
+    bool add_dependency = TRY(prompt_yes_no("Add more dependencies?", !add_defaults));
+    if (!add_dependency)
+      return {};
+
+    Dependency dependency;
+    std::string source_string = TRY(prompt<std::string>("source [git, local]", "git"));
+    Dependency::Source source = Dependency::Source::GIT;
+    if (source_string == "git" || source_string == "GIT" || source_string == "Git")
+      source = Dependency::Source::GIT;
+    else if (source_string == "local" || source_string == "LOCAL" || source_string == "Local")
+      source = Dependency::Source::LOCAL;
+    else
+      return cpp::fail(Error(Error::InputError, "must be git or local"));
+
+    dependency.source = source;
+
+    switch (source) {
+      case Dependency::Source::GIT: {
+        std::string name = TRY(prompt<std::string>("name"));
+        dependency.name = name;
+        std::string location = TRY(prompt<std::string>("URL"));
+        dependency.location = location;
+        std::string version = TRY(prompt<std::string>("version or tag", "master"));
+        dependency.version = version;
+        break;
+      }
+      case Dependency::Source::LOCAL: {
+        std::string name = TRY(prompt<std::string>("name"));
+        dependency.name = name;
+        std::string location = TRY(prompt<std::string>("path"));
+        dependency.location = location;
+        dependency.version = "LOCAL";
+        break;
+      }
+    }
+
+    project_info.dependencies.push_back(dependency);
+  }
+
   return {};
 }
 
