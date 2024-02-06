613b17432320ddd76219fbf915cc36553d755e3b
karei <eetu.kivelae@gmail.com>
2024-02-06 18:36:28 +0200

    Fix Result default dependency source

--- src/defaults.h
+++ src/defaults.h
@@ -11,7 +11,7 @@ constexpr bool DEFAULT_ADD_DEPENDENCIES = true;
 constexpr std::array<Dependency, 2> DEFAULT_DEPENDENCIES() {
   return {
           Dependency{.name = "fmt", .location = "https://github.com/fmtlib/fmt.git", .version = "10.1.0", .source = Dependency::Source::GIT},
-          Dependency{.name = "Result", .location = "https://github.com/bitwizeshift/result.git", .version = "master", .source = Dependency::Source::LOCAL}};
+          Dependency{.name = "Result", .location = "https://github.com/bitwizeshift/result.git", .version = "master", .source = Dependency::Source::GIT}};
 }
 
 }// namespace haru
