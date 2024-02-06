c57ea7c1a06ddcd14694a48e41d5b13a03944d71
karei <eetu.kivelae@gmail.com>
2024-02-06 16:49:01 +0200

    Handle default dependencies

---
+++ src/defaults.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "project_info.h"
+#include <array>
+
+namespace haru {
+constexpr const char* DEFAULT_CMAKE_VERSION = "3.28";
+constexpr const char* DEFAULT_VERSION = "0.1.0";
+constexpr const char* DEFAULT_STD_VERSION = "23";
+constexpr bool DEFAULT_ADD_DEPENDENCIES = true;
+
+constexpr std::array<Dependency, 2> DEFAULT_DEPENDENCIES() {
+  return {
+          Dependency{.name = "fmt", .location = "https://github.com/fmtlib/fmt.git", .version = "10.1.0", .source = Dependency::Source::GIT},
+          Dependency{.name = "Result", .location = "https://github.com/bitwizeshift/result.git", .version = "master", .source = Dependency::Source::LOCAL}};
+}
+
+}// namespace haru
--- src/project_info.cpp
+++ src/project_info.cpp
@@ -1,7 +1,11 @@
 #include "project_info.h"
+#include "defaults.h"
 #include "utils.h"
+#include <sstream>
 
 namespace haru {
+cpp::result<void, Error> handle_adding_dependencies(ProjectInfo& project_info);
+
 cpp::result<ProjectInfo, Error> ProjectInfo::parse_from_input(std::optional<std::string> default_name) {
   ProjectInfo project_info;
   auto project_name_ret = prompt<std::string>("Project name", default_name);
@@ -9,23 +13,56 @@ cpp::result<ProjectInfo, Error> ProjectInfo::parse_from_input(std::optional<std:
     return cpp::fail(project_name_ret.error());
   project_info.name = project_name_ret.value();
 
-  auto cmake_version_ret = prompt<std::string>("Minimum cmake version", "3.28");
+  auto cmake_version_ret = prompt<std::string>("Minimum cmake version", DEFAULT_CMAKE_VERSION);
   if (cmake_version_ret.has_error())
     return cpp::fail(cmake_version_ret.error());
   project_info.cmake_version = cmake_version_ret.value();
 
-  auto version_ret = prompt<std::string>("version", "0.1.0");
+  auto version_ret = prompt<std::string>("version", DEFAULT_VERSION);
   if (version_ret.has_error())
     return cpp::fail(version_ret.error());
   project_info.version = version_ret.value();
 
-  auto standard_ret = prompt<std::string>("C++-standard", "20");
+  auto standard_ret = prompt<std::string>("C++-standard", DEFAULT_STD_VERSION);
   if (standard_ret.has_error())
     return cpp::fail(standard_ret.error());
   project_info.standard = standard_ret.value();
 
+  auto add_dependencies_ret = prompt_yes_no("Add dependencies?", DEFAULT_ADD_DEPENDENCIES);
+  if (add_dependencies_ret.has_error())
+    return cpp::fail(add_dependencies_ret.error());
+
+  if (add_dependencies_ret.value()) {
+    auto added_dependencies_ret = handle_adding_dependencies(project_info);
+    if (added_dependencies_ret.has_error())
+      return cpp::fail(added_dependencies_ret.error());
+  }
 
   return project_info;
+}
+
+cpp::result<void, Error> handle_adding_dependencies(ProjectInfo& project_info) {
+  auto default_dependencies = DEFAULT_DEPENDENCIES();
+  std::stringstream defaults_formatted;
+  size_t last_index = default_dependencies.size() - 1;
+  for (size_t i = 0; i < last_index; i++)
+    defaults_formatted << fmt::format("{}, ", default_dependencies[i].name);
+  if (last_index >= 0)
+    defaults_formatted << default_dependencies[last_index].name;
+
+
+  auto add_defaults_ret = prompt_yes_no(
+          fmt::format("Add default dependencies? [{}]", defaults_formatted.str()).c_str(),
+          DEFAULT_ADD_DEPENDENCIES);
+  if (add_defaults_ret.has_error())
+    return cpp::fail(add_defaults_ret.error());
+
+  if (add_defaults_ret.value()) {
+    spdlog::info("Adding defaul dependencies");
+    project_info.dependencies = std::vector<Dependency>(default_dependencies.begin(), default_dependencies.end());
+  }
+
+  return {};
 }
 
 }// namespace haru
