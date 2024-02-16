d6d303fa962ff4a4a5e3d32cedad722b6d341b51
karei <eetu.kivelae@gmail.com>
2024-02-16 02:32:15 +0200

    Compile default files into program binary
    Default files that can be included in the generated project folder are
    now compiled into the haru binary.
    
    Purpose behind this is allowing haru to remain as a single file
    executable with no external dependencies.
    
    With this method any changes to these preset files can be made inside
    `assets/` and CMake will issue a rebuild of the generated .cpp files on
    any changes.

---
+++ src/bake_in.h
@@ -0,0 +1,8 @@
+#pragma once
+
+extern unsigned char bake_in_clang_format[];
+extern unsigned int bake_in_clang_format_len;
+extern unsigned char bake_in_cmake_format[];
+extern unsigned int bake_in_cmake_format_len;
+extern unsigned char bake_in_gitignore[];
+extern unsigned int bake_in_gitignore_len;
