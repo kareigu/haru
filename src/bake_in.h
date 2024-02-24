5f55abe442ca185deb6018dc29347cec77a7a2cc
karei <eetu.kivelae@gmail.com>
2024-02-24 17:02:55 +0200

    Use macro to define generated content variables

--- src/bake_in.h
+++ src/bake_in.h
@@ -1,8 +1,9 @@
 #pragma once
 
-extern unsigned char bake_in_clang_format[];
-extern unsigned int bake_in_clang_format_len;
-extern unsigned char bake_in_gersemirc[];
-extern unsigned int bake_in_gersemirc_len;
-extern unsigned char bake_in_gitignore[];
-extern unsigned int bake_in_gitignore_len;
+#define BAKE_IN(NAME)                    \
+  extern unsigned char bake_in_##NAME[]; \
+  extern unsigned int bake_in_##NAME##_len
+
+BAKE_IN(clang_format);
+BAKE_IN(gersemirc);
+BAKE_IN(gitignore);
