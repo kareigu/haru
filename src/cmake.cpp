a34cdcb6a3bdf8703b760efa1d9bbe42be36b4c5
karei <mail@karei.dev>
2024-04-20 13:12:36 +0300

    Override local config in `cmake::init()` when `-g` is present

--- src/cmake.cpp
+++ src/cmake.cpp
@@ -29,11 +29,12 @@ namespace cmake {
   }
 
   std::expected<void, Error> handle_init_command(const Command::Flags_t flags) {
+    bool override_local = flags & Command::Flags::GLOBAL;
     Config config;
-    config.cpp_compiler = TRY(haru::Config::get_value("cmake.cpp_compiler"));
-    config.c_compiler = TRY(haru::Config::get_value("cmake.c_compiler"));
-    config.generator = TRY(haru::Config::get_value("cmake.generator"));
-    config.build_dir = TRY(haru::Config::get_value("cmake.build_dir"));
+    config.cpp_compiler = TRY(haru::Config::get_value("cmake.cpp_compiler", override_local));
+    config.c_compiler = TRY(haru::Config::get_value("cmake.c_compiler", override_local));
+    config.generator = TRY(haru::Config::get_value("cmake.generator", override_local));
+    config.build_dir = TRY(haru::Config::get_value("cmake.build_dir", override_local));
 
     log::info("CMake settings:\n{}", config);
 
